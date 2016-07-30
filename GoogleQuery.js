6262903152cc77c34d9fb9616dfab94804fc16cd
Duy Nguyen <lucky7duy211@gmail.com>
2016-07-29 22:38:12 -0700

    Skeleton completed

--- GoogleQuery.js
+++ GoogleQuery.js
@@ -3,6 +3,7 @@
  */
 
 var Horsemen = require('node-horseman');
+var async = require('async');
 
 var rider = new Horsemen(
     {
@@ -13,10 +14,30 @@ var rider = new Horsemen(
 rider
     .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
     .open('https://www.google.com/search?q=innocence')
+    .status()
+    .then(function (result)
+    {
+        if (result !== 200)
+        {
+            console.log("Website does NOT load!")
+            rider.close();
+        }
+
+
+        else
+        {
+            console.log("Website loads!");
+
+        }
+    })
     .click('#uid_0 > div._LJ._qxg.xpdarr._WGh.vk_arc')
     .keyboardEvent('keypress', 16777221)
-    .text('.xpdxpnd > div > span:first')
+    .text('.xpdxpnd >> span:first')
     .log() // prints out the number of results
+    .catch(function (err)
+    {
+        console.log(err);
+    })
     .close();
 
 
